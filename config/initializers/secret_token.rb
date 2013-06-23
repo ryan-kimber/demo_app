241f52644c0455e72b48de863f72679ecd31767b
Ryan Kimber <ryan.kimber@patientordersets.com>
2013-06-23 10:49:48 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = 'f189360698bb6963cda8535e00854964458d995b51ff1a9ab54cc2a704e846f6dd348a0852e680fcdc962daa79b0f41bfbbe7b84cd919873672bcbd0b458bab1'
